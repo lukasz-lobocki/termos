cf7b51a8eabd3711a252bddde29b1a0f1797c492
Lukasz Lobocki <lukasz.lobocki@googlemail.com>
2025-08-19 07:53:12 +0200

    feat: Reveal line number in error handlilng.

--- cmd/main.go
+++ cmd/main.go
@@ -89,6 +89,6 @@ func initLoggers() {
 	hiRed := color.New(color.FgHiRed).SprintFunc()
 
 	logInfo = log.New(os.Stderr, hiCyan("╭info\n╰"), 0)
-	logWarning = log.New(os.Stderr, hiYellow("╭warning\n╰"), log.Lshortfile)
-	logError = log.New(os.Stderr, hiRed("╭error\n╰"), log.Lshortfile)
+	logWarning = log.New(os.Stderr, hiYellow("╭warning\n╰"), 0)
+	logError = log.New(os.Stderr, hiRed("╭error\n╰"), 0)
 }
--- cmd/shot.go
+++ cmd/shot.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"os"
 	"os/exec"
+	"runtime"
 
 	"github.com/creack/pty"
 	"github.com/fogleman/gg"
@@ -44,27 +45,28 @@ func init() {
 }
 
 func shot(args []string) {
-	var buf bytes.Buffer
-	printout := get_printout(TERMINAL_ROWS, TERMINAL_COLS, args[0], args[1:]...) // agr0 is the command to be run
-	_, err := io.Copy(&buf, printout)                                            // read the stream, memorize it in the buffer
-	check(err)
-	save_stream(buf.Bytes(), SAVED_STREAM_FILENAME) // save it
+	buf := get_printout(TERMINAL_ROWS, TERMINAL_COLS, args[0], args[1:]...) // agr0 is the command to be run
+	save_stream(buf.Bytes(), SAVED_STREAM_FILENAME)                         // save it
 	graph(&buf)
 }
 
-func get_printout(rows uint16, cols uint16, cmd_name string, cmd_args ...string) *os.File {
+func get_printout(rows uint16, cols uint16, cmd_name string, cmd_args ...string) bytes.Buffer {
+	var b bytes.Buffer
 	w := pty.Winsize{Rows: rows, Cols: cols}
 	c := exec.Command(cmd_name, cmd_args...)
 	f, err := pty.StartWithSize(c, &w) // get command output file from the (pty) pseudo-terminal
 	check(err)
-	return f
+	io.Copy(&b, f) // read the stream, memorize it in the buffer
+	return b
 }
 
 func graph(in io.Reader) {
 	parsed, err := bunt.ParseStream(in)
 	check(err)
 	logInfo.Println(parsed.String())
-	check(err)
+	// for _, cr := range *parsed {
+	// 	logInfo.Print(cr)
+	// }
 	dc := gg.NewContext(1000, 1000)
 	dc.DrawCircle(500, 500, 400)
 	dc.SetRGB(0, 0, 0)
@@ -75,7 +77,12 @@ func graph(in io.Reader) {
 
 func check(e error) {
 	if e != nil {
-		logError.Fatal(e)
+		_, file, line, ok := runtime.Caller(1)
+		if ok {
+			logError.Fatalf("File: %s Line:%v Message:%+v", file, line, e)
+		} else {
+			logError.Fatalf("Message:%+v", e)
+		}
 	}
 }
 
